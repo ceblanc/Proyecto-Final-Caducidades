4e78531a13d685426b66beecb08f0eb3523553a3
Ceblanc <carlosblancomartin@gmail.com>
2023-02-01 18:54:38 +0100

    Navegación entre páginas

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ app = Flask(__name__) #En app se encuentra el servidor web de Flask
 @app.route('/')
 def home():
     return render_template("index.html")
+@app.route('/informes')
+def informes():
+    return render_template("informes.html")
+
+@app.route('/articulos')
+def articulos():
+    return 'articulos'
+@app.route('/fechas')
+def fechas():
+    return 'fechas'
 
 if __name__ == '__main__':
     app.run(debug=True) #El debug=True hace que cada vez que reiniciemos el servidor o modifiquemos código, el servidor de Flask se reinicie solo
